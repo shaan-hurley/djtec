1a9513c8b75c9e1ee74495da67df137ea7a890f8
Shaan <Shaan@admins-MacBook-Pro.local>
2020-03-09 12:00:51 -0700

    renders homepage

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const app = express();
 const port = 3000;
 const exphbs = require('express-handlebars');
 
-app.get('/', (req, res)=> res.send('hello world'))
+
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res)=> {
+    res.render('home')
+})
 
 app.listen(port, () => {
     console.log('Reddit listening on port localhost:3000!');
