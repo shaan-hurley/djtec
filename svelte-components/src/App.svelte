594a63f4ded252109eac5b64b20b15d20fef3214
Shaan <Shaan@admins-MacBook-Pro.local>
2020-03-18 12:22:39 -0700

    svelte components

--- svelte-components/src/App.svelte
+++ svelte-components/src/App.svelte
@@ -1,31 +1,33 @@
 <script>
-	let name = 'Shaan';
-	
+  import Player from "./Player.svelte";
+  import AddPlayer from "./AddPlayer.svelte";
+
+  let players = [
+
+  ];
+
+  const addPlayer = e => {
+    const newPlayer = e.detail;
+    players = [...players, newPlayer];
+  };
+ 
+  const removePlayer = e => {
+    players = players.filter(player => player.name !== e.detail);
+  };
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	
+<main> 
+	<div class="container">
+	<AddPlayer on:addplayer={addPlayer} />
+	{#if players.length === 0}
+		<p>No Songs</p>
+	{:else}
+		{#each players as player}
+		<Player
+			name={player.name}
+			points={player.points}
+			on:removeplayer={removePlayer} />
+		{/each}
+	{/if}
+	</div>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
