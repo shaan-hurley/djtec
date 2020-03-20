6679e5ade04038cbef4b91b523f89e6a01624dfb
Shaan <Shaan@admins-MacBook-Pro.local>
2020-03-19 18:05:43 -0700

    displays playlists and songs

--- config/sendrequest.js
+++ config/sendrequest.js
@@ -16,11 +16,114 @@ function addFriend(name) {
     })
 }
 
+function getSongsForPlaylist(url,id){
+    let spotifytoken = $('#user_token1').val()
+        $.ajax({
+            url: url,
+            type: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + spotifytoken
+            },
+            success: function(data) {
+                console.log("checking songs data",data);
+                let returnStr = '';
+                returnStr += displaySongsString(data);
+                console.log("checking returnStr",returnStr);
+                $(`#${id} .songs`).replaceWith(returnStr);
+                $(`#${id} .songs`).show();
+            
+            }
+        });
+}
+
+function addEventListenerToPlaylist(){
+    $('.playlist').on('click', function(e){
+        console.log("checking e",e);
+        let id = e.target.id;
+        let url = e.target.dataset.songsurl;
+        console.log("checking url/id",url,id);
+       getSongsForPlaylist(url,id);
+    });
+}
+
+function getPlaylistData(){
+    let spotifytoken = $('#user_token1').val()
+        $.ajax({
+            url: 'https://api.spotify.com/v1/me/playlists',
+            type: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + spotifytoken
+            },
+            success: function(data) {
+               
+            }
+        }).done(function(data){
+            let htmlstr = '';
+            htmlstr += displayPlaylistsString(data);
+            $('#result').append(htmlstr);
+            addEventListenerToPlaylist();
+            return htmlstr;
+        });
+        // $('#reload').load(location.href + ' #reload');
+}
+
+function displaySongsString(data){
+    console.log("checking data in displaysongsstring",data);
+    let returnStr = '';
+     data.items.forEach(item =>{
+         let imgUrl = item.track.album.images.length > 0 ? item.track.album.images[0].url : "";
+        returnStr += `
+        <li>
+            <img class="header-image"  style="height:100px;width:100px"src='${imgUrl}' alt="logo">
+            <h5>${item.track.name}</h>
+            <h6>by:${item.track.artists[0].name}</h6>
+            <button 
+                data-uri="${item.track.uri}"
+                type="submit" 
+                class="btn btn-success change_song">Accept</button>
+        <li>`
+    })
+
+    return returnStr;
+}
+//this function takes the data returned from playlists call,  and makes a row for each playlist
+function displayPlaylistsString(data){
+    let returnStr = '';
+    
+    data.items.forEach(item =>{
+
+        returnStr += `
+        <div class="row ">
+        <div class="card" id="${item.id}" >
+            <div class="card-body" style="color:black">
+                <h4 class="card-title">
+                ${item.name}
+                </h4>
+                <img class="card-img-top playlist" src="${item.images[0].url}" style="width:250px; height:250px; border-radius: 50%;" alt="User Profile" id="${item.id}" data-songsUrl="${item.tracks.href}">
+                <h6 class="card-subtitle mb-2 text-muted">${item.tracks.total} songs in this playlist</h6>
+
+                <ul class="songs" style="display:none">
+
+                </ul>
+            </div>
+        </div>
+        </div>`
+        
+    });
+   
+    return returnStr;
+}
+
 
 $(document).ready(function() {
     $('.friend-add').on('click', function(e) {
         e.preventDefault();
     });
+   
     $('#accept_friend').on('click', function() {
         var senderId = $('#senderId').val();
         var senderName = $('#senderName').val();
@@ -88,7 +191,7 @@ $(document).ready(function() {
         $('#reload').load(location.href + ' #reload');
     });
     $('body').on('click', '.change_song', function(e) {
-        let spotifytoken = $('#friend_token').val()
+        let spotifytoken = $('#user_token2').val()
         let song = $(this).data('uri')
 
         console.log(spotifytoken)
@@ -107,37 +210,10 @@ $(document).ready(function() {
         });
         $('#reload').load(location.href + ' #reload');
     });
-    $('#get_playlist').on('click', function() {
-        let spotifytoken = $('#user_token1').val()
-
-        $.ajax({
-            url: 'https://api.spotify.com/v1/me/playlists',
-            type: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + spotifytoken
-            },
-            success: function(data) {
-                console.log('*******************')
-                console.log(data);
-                let htmlstr = ''
-                    // data.tracks.items.forEach(song => {
-                    //     htmlstr += `
-                    //     <div class="row">
-                    //         <img class="header-image" src='${song.album.images[2].url}' alt="logo">
-                    //         <h5>${song.name}</h>
-                    //         <h6>by:${song.artists[0].name}</h6>
-                    //         <button 
-                    //             data-uri="${song.uri}"
-                    //             type="submit" 
-                    //             class="btn btn-success change_song">Accept</button>
-                    //     </div>`
-                    // });
-
-                // el.innerHTML = htmlstr
-            }
-        });
-        $('#reload').load(location.href + ' #reload');
-    });
+    
+  
+    getPlaylistData();
+    
+   
+    
 });
