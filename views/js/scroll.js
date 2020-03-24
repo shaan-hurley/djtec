bc426d11ec67710f7de5091a4ca3adf575bd9c35
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-03-23 18:23:11 -0700

    landing page working

--- views/js/scroll.js
+++ views/js/scroll.js
@@ -151,3 +151,80 @@ $(document).ready(function () {
     });
 
 });
+
+
+
+
+//this is where we apply opacity to the arrow
+$(window).scroll(function () {
+
+    //get scroll position
+    var topWindow = $(window).scrollTop();
+    //multipl by 1.5 so the arrow will become transparent half-way up the page
+    var topWindow = topWindow * 1.5;
+
+    //get height of window
+    var windowHeight = $(window).height();
+
+    //set position as percentage of how far the user has scrolled 
+    var position = topWindow / windowHeight;
+    //invert the percentage
+    position = 1 - position;
+
+    //define arrow opacity as based on how far up the page the user has scrolled
+    //no scrolling = 1, half-way up the page = 0
+    $('.arrow-wrap').css('opacity', position);
+
+});
+
+
+
+
+//IIFE
+(function () {
+    "use strict";
+    var menuId;
+    function init() {
+        menuId = document.getElementById("menu");
+        document.addEventListener("scroll", scrollMenu, false);
+    }
+    function scrollMenu() {
+        if (document.documentElement.scrollTop > 50) {
+            menuId.classList.add("scroll");
+            console.log('scroll');
+        }
+        else {
+            menuId.classList.remove("scroll");
+            console.log('no-scroll');
+        }
+    }
+    document.addEventListener("DOMContentLoaded", init, false);
+})();
+
+(function () {
+    "use strict";
+    var mobBtn, topMenu;
+
+    function init() {
+        mobBtn = document.getElementById("mobile-btn");
+        topMenu = document.getElementById("top-menu");
+        mobBtn.addEventListener("click", mobileMenu, false);
+    }
+
+    function mobileMenu() {
+        if (topMenu.classList.contains("mobile-open")) {
+            topMenu.classList.remove("mobile-open");
+        } else {
+            topMenu.classList.add("mobile-open");
+        }
+        if (mobBtn.classList.contains("hamburger-cross")) {
+            mobBtn.classList.remove("hamburger-cross");
+        }
+        else {
+            mobBtn.classList.add("hamburger-cross");
+        }
+    }
+
+    document.addEventListener("DOMContentLoaded", init);
+
+})();
